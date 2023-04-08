37168ce4da2e18659452fb239ceb0e1698812c6e
ccp84 <ccp84@icloud.com>
2023-04-08 12:31:53 +0100

    jsx>fix missing return

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import React from "react";
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client";
 import Pet from "./Pet";
 
 const App = () => {
-  <div>
-    <h1>Adopt Me!</h1>
-    <Pet name="Obi" animal="Dog" breed="Poodle" />
-    <Pet name="Fizz" animal="Cat" breed="Persian" />
-    <Pet name="Nala" animal="Hamster" breed="Sirian" />
-  </div>;
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Obi" animal="Dog" breed="Poodle" />
+      <Pet name="Fizz" animal="Cat" breed="Persian" />
+      <Pet name="Nala" animal="Hamster" breed="Sirian" />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");

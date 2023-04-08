d714baebae4e53daba2722b8c6e746e03562335f
ccp84 <ccp84@icloud.com>
2023-04-08 11:05:47 +0100

    jsx>add pet module

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      animal: "Dog",
-      name: "Obi",
-      breed: "Poodle",
-    }),
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Willow",
-      breed: "Fluffy",
-    }),
-    React.createElement(Pet, {
-      animal: "Nala",
-      name: "Hamster",
-      breed: "Sirian",
-    }),
-  ]);
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Obi" animal="Dog" breed="Poodle" />
+    <Pet name="Fizz" animal="Cat" breed="Persian" />
+    <Pet name="Nala" animal="Hamster" breed="Sirian" />
+  </div>;
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);

235b87c1855cb9caed92828d7ec24f9b4aad51e1
ccp84 <ccp84@icloud.com>
2023-04-09 13:32:47 +0100

    jsx>fix bugs in api call

--- src/useBreedList.jsx
+++ src/useBreedList.jsx
@@ -20,7 +20,7 @@ export default function useBreedList(animal) {
       setStatus("Loading");
 
       const res = await fetch(
-        `http://pets.v2.dev-apis.com/breeds?animal=${animal}`
+        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
       );
       const json = await res.json();
       localCache[animal] = json.breeds || [];

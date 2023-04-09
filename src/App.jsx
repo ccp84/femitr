71e16335f8a3f433b8c8cc3a24311a25764b4c67
ccp84 <ccp84@icloud.com>
2023-04-09 21:21:23 +0100

    jsx>instantiate query client

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import { createRoot } from "react-dom/client";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">
-          <h1>Adopt Me!</h1>
-        </Link>
-      </header>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">
+            <h1>Adopt Me!</h1>
+          </Link>
+        </header>
 
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };

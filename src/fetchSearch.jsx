a8ce11cb2297819c93a1db05184160431e34a6ee
ccp84 <ccp84@icloud.com>
2023-04-10 21:00:45 +0100

    build>update to https

--- src/fetchSearch.jsx
+++ src/fetchSearch.jsx
@@ -1,7 +1,7 @@
 async function fetchSearch({ queryKey }) {
   const { animal, breed, location } = queryKey[1];
   const res = await fetch(
-    `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    `https://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
   );
   if (!res.ok)
     throw new Error(`pet search not okay: ${animal}, ${location}, ${breed}`);
